c712060bcb27aa37538118339e10a433b084732a
naotoyoshimoto <namusic7010@gmail.com>
2019-03-19 10:45:38 +0900

    add test whather indicated path is wrong

--- data/file_test.go
+++ data/file_test.go
@@ -1,11 +1,21 @@
 package data
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestGetDataFilePath(t *testing.T) {
-	_, err := GetDataFilePath()
+	filepath, err := GetDataFilePath()
 
 	if err != nil {
 		t.Fatalf("can't get data file path")
 	}
+
+	home := os.Getenv("HOME")
+
+	if filepath != home+"/.ohp" {
+		t.Fatalf("filepath is wrong")
+	}
+
 }
