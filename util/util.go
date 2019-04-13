c0e2ea91f3aa5f71ba0b236d8b8d8920c94baba9
naotoyoshimoto <namusic7010@gmail.com>
2019-04-13 23:45:32 +0900

    add util package for load env

---
+++ util/util.go
@@ -0,0 +1,14 @@
+package util
+
+import (
+	"fmt"
+
+	"github.com/joho/godotenv"
+)
+
+func LoadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env fileohp")
+	}
+}
