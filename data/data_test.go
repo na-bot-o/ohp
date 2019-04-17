7c119360f104263bb01b68fdea80b23a720a6dd1
naotoyoshimoto <namusic7010@gmail.com>
2019-04-18 00:33:36 +0900

    add getpage test

--- data/data_test.go
+++ data/data_test.go
@@ -15,3 +15,36 @@ func TestGetDataFilePath(t *testing.T) {
 	}
 
 }
+func TestGetPages(t *testing.T) {
+	expectedInputs := []struct {
+		Name string
+		Tag  string
+		Url  string
+	}{
+		{"Amazon", "Ecommerce", "https://amazon.com"},
+		{"Google", "SearchEngine", "https://google.com"},
+		{"Twitter", "SNS", "https://twitter.com"},
+		{"Yahoo", "SearchEngine", "https://yahoo.co.jp"},
+	}
+
+	dataStub := Data{".ohp_test", "../.ohp_test"}
+
+	pages, err := dataStub.GetPages()
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	for _, expectedInput := range expectedInputs {
+		for index, page := range pages {
+			if expectedInput == page {
+				break
+			}
+			if index == len(pages)-1 {
+				t.Errorf("not found expected value : %+v", page)
+			}
+		}
+	}
+
+}
